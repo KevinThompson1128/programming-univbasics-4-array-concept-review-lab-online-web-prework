814587e4b01a062b54f1694cac0f64a710df33e1
Kevin Thompson <indelible11@yahoo.com>
2019-10-27 18:25:56 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -8,8 +8,7 @@
 
 def find_element_index(array, value_to_find)
   # Add your solution here
-  array.index(0, 1)
-
+array.find_index(value_to_find)
 def find_min_value(array)
   # Add your solution here
 end
